175df42c0a4b585a37d664676f26873adaa44fc6
Sucharita Halder <sucharitahalder729@gmail.com>
2024-02-08 09:11:04 +0530

    added get size , is empty and is full method

--- QueueUsingArray.java
+++ QueueUsingArray.java
@@ -1,5 +1,6 @@
 class Queue{
-    int[] queue = new int[5];
+    int capacity = 5;
+    int[] queue = new int[capacity];
     int size = 0;
     int front = 0;
     int rear = 0;
@@ -7,46 +8,60 @@ Queue(){
 
 }
 public void enqueue(int data){
+    if(!queueisFull()){
     queue[rear] = data;
-    rear = (rear+1)%5;
+    rear = (rear+1)%capacity;
     size++;
+}else{
+    System.out.println("Queue is full.Cannot Enqueue.");
+}
 }
 public void show(){
     System.out.println("Elements: ");
     for(int i = 0;i<size;i++){
-        System.out.print(queue[(i+front)%5]+ " ");
+        System.out.print(queue[(i+front)%capacity]+ " ");
     }
     System.out.println(" ");
-    System.out.println("The whole queue :");
+    System.out.println("The whole queue array :");
     for(int n :queue){
         System.out.print(n + " ");
     }
     System.out.println(" ");
 }
 public void dequeue(){
-    if(size == 0){
+    if(queueisEmpty()){
         System.out.println("Queue is empty. Cannot dequeue.");
-        return;
-    }
-    
+    }else{
     int data = queue[front];
-    front = (front+1)%5;
+    front = (front+1)%capacity;
     size--;
     System.out.println("Dequeued element: " + data);
 }
-
+}
+public int getsize(){
+    return size;
+}
+public boolean queueisEmpty(){
+    return getsize()== 0;
+}
+public boolean queueisFull(){
+    return getsize() == capacity;
+}
 }public class QueueUsingArray {
     public static void main(String[] args){
         Queue myqueue1 = new Queue();
-        myqueue1.enqueue(52);
-        myqueue1.enqueue(55);
-        myqueue1.enqueue(69);
-        myqueue1.enqueue(74);
+        // myqueue1.enqueue(52);
+        // myqueue1.enqueue(55);
+        // myqueue1.enqueue(69);
+        // myqueue1.enqueue(74);
         myqueue1.dequeue();
         myqueue1.dequeue();
-        myqueue1.enqueue(80);
-        myqueue1.enqueue(82);
+        // myqueue1.enqueue(80);
+        // myqueue1.enqueue(82);
         myqueue1.show();
+        System.out.println("Size is :" + myqueue1.getsize());
+        System.out.println("Is the queue Empty ?"+ myqueue1.queueisEmpty());
+        System.out.println("Is the queue Full ?"+ myqueue1.queueisFull());
 
     }
 }
